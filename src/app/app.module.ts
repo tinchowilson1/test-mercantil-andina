139c215d866bce6e8a7a317bbcdc532c3981519e
mbazan <mbazan@invoition.com>
2021-05-28 19:18:34 -0300

    limpieza

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
@@ -8,14 +7,9 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-import { NgWizardConfig, NgWizardModule, THEME } from 'ng-wizard';
 import { SharedModule } from './utils/shared.module';
 import { HeaderModule } from './templates/header/header.module';
 import { FooterModule } from './templates/footer/footer.module';
-
-const ngWizardConfig: NgWizardConfig = {
-  theme: THEME.arrows
-};
 
 @NgModule({
   declarations: [
@@ -23,13 +17,11 @@ const ngWizardConfig: NgWizardConfig = {
   ],
   imports: [
     SharedModule,
-    // CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
-    NgWizardModule.forRoot(ngWizardConfig),
     HeaderModule,
     FooterModule
   ],
